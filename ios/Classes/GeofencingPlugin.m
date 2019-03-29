c7536b83f0edca3c74567cc993016012759ca365
Amar Ubhe <ubheamar@gmail.com>
2019-03-29 19:41:41 +0400

    Converted Swift To Objecttive C

--- ios/Classes/GeofencingPlugin.m
+++ ios/Classes/GeofencingPlugin.m
@@ -163,8 +163,8 @@ monitoringDidFailForRegion:(CLRegion *)region
     _locationManager = [[CLLocationManager alloc] init];
     [_locationManager setDelegate:self];
     [_locationManager requestAlwaysAuthorization];
-    _locationManager.allowsBackgroundLocationUpdates = YES;
-    _locationManager.pausesLocationUpdatesAutomatically = false;
+    [_locationManager allowsBackgroundLocationUpdates:YES];
+    [_locationManager pausesLocationUpdatesAutomatically:YES];
     
     _headlessRunner = [[FlutterEngine alloc] initWithName:@"geofencing_plugin" project:nil];
     _registrar = registrar;
